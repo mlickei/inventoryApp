ff73eaf3d5e0c738708d5672ba48a6babd4eda7c
Matthew Lickei <mjlickei93@gmail.com>
2018-04-11 21:11:12 -0500

    Started on edit item view.

---
+++ app/src/main/java/matthew/inventoryapp/activity/EditItemView.kt
@@ -0,0 +1,24 @@
+package matthew.inventoryapp.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import matthew.inventoryapp.R
+
+import kotlinx.android.synthetic.main.activity_edit_item_view.*
+
+class EditItemView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_item_view)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
