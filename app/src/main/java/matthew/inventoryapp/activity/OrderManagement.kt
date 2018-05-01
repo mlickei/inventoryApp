3e2463dce9293cdc8018c2c1cac5327f78585604
Matthew Lickei <mjlickei93@gmail.com>
2018-04-30 22:05:30 -0500

    Hooked items recyler view into order management activity.

--- app/src/main/java/matthew/inventoryapp/activity/OrderManagement.kt
+++ app/src/main/java/matthew/inventoryapp/activity/OrderManagement.kt
@@ -1,11 +1,22 @@
 package matthew.inventoryapp.activity
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.ActionBar
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_items.*
 import matthew.inventoryapp.R
+import matthew.inventoryapp.order.Order
+import matthew.inventoryapp.order.OrderViewModel
+import matthew.inventoryapp.view.OrdersRecylerViewAdapter
 
 class OrderManagement : BaseActivity() {
+
+    var orders: ArrayList<Order> = ArrayList()
+    lateinit var ordersRecyclerViewAdapter: RecyclerView.Adapter<OrdersRecylerViewAdapter.ViewHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +24,44 @@ class OrderManagement : BaseActivity() {
         setSupportActionBar(toolbar)
         val actionBar: ActionBar? = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        addItem.setOnClickListener { view ->
+            //TODO
+        }
+
+        setupRecyclerView()
     }
 
+    private fun setupRecyclerView() {
+        //Get Listing View Content Container
+        var listingViewContent: View = findViewById(R.id.ordersInclude)
+
+        var ordersRecyclerViewLayoutManager = LinearLayoutManager(this)
+        ordersRecyclerViewAdapter = OrdersRecylerViewAdapter(this.orders, this)
+
+        var ordersRecyclerView: RecyclerView = listingViewContent.findViewById(R.id.ordersList)
+        ordersRecyclerView.layoutManager = ordersRecyclerViewLayoutManager
+        ordersRecyclerView.adapter = ordersRecyclerViewAdapter
+    }
+
+    private fun setupOrdersViewModel() {
+        var ordersViewModel: OrderViewModel = ViewModelProviders.of(this).get(OrderViewModel::class.java)
+        ordersViewModel.orders.observe(this, Observer { orders ->
+
+        })
+    }
+
+    private fun handleOrdersListUpdate(orders:List<Order>) {
+        this.orders.clear()
+
+        if(orders != null) {
+            this.orders.addAll(orders)
+        }
+
+        updateOrdersListView()
+    }
+
+    private fun updateOrdersListView() {
+        ordersRecyclerViewAdapter.notifyDataSetChanged()
+    }
 }
