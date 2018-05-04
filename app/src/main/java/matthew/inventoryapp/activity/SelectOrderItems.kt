abc9e2352cc531926a9952013082250bcb405837
Matthew Lickei <mjlickei93@gmail.com>
2018-05-03 20:52:27 -0500

    Added select order items view.

---
+++ app/src/main/java/matthew/inventoryapp/activity/SelectOrderItems.kt
@@ -0,0 +1,13 @@
+package matthew.inventoryapp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import matthew.inventoryapp.R
+
+class SelectOrderItems : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_order_items)
+    }
+}
